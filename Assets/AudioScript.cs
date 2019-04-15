a103d2db31aef3a1400b0fe5f82807e830093f39
Ayush Bhardwaj <Ayush12072@users.noreply.github.com>
2019-04-15 09:10:43 +0530

    Zombie Changes

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip otherClip;
+    AudioSource audioSource;
+    public GameObject gameOverPanel;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = otherClip;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(audioSource.isPlaying);
+        if(!audioSource.isPlaying){
+            
+            gameOverPanel.SetActive(true);  
+        }
+    }
+}
---
+++ Assets/UiManagerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManagerScript : MonoBehaviour
+{
+    //public static UiManager instance;
+    public GameObject startPanel;
+    public GameObject gameOverPanel;
+    
+
+    void Awake(){
+        // if(instance == null){
+        //     instance = null;
+        // }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    public void GameStart(){
+        //startPanel.GetComponent<Animator>().Play("landscape");
+    }
+
+    public void GameOver(){
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Reset(){
+        SceneManager.LoadScene(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
